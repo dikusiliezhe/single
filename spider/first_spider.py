6ccb1c28c5241e78e03bf18c203084d6071f474a
dikusiliezhe <133321449@qq.com>
2023-06-28 17:22:18 +0800

    qimai test

--- spider/first_spider.py
+++ spider/first_spider.py
@@ -11,7 +11,7 @@ import time
 sys.path.append(os.path.abspath(os.path.dirname(__file__)).split('spider')[0])
 from config.all_config import *
 from string import Template
-import ddddocr
+# import ddddocr
 from urllib.parse import quote
 import hmac
 from filestream_y.FileStream_y import stream_type
@@ -21,8 +21,8 @@ class first_spider(Manager):
     name = 'first_spider'
     custom_settings = {
         # 'retry_http_codes': [202, 412],
-        'Waiting_time': 10,
-        'IS_PROXY': False,
+        'Waiting_time': 1,
+        'IS_PROXY': True,
         'IS_SAMEIP': False,
         'UA_PROXY': False,
         # 'X_MAX_PRIORITY': 15,
@@ -36,7 +36,7 @@ class first_spider(Manager):
         self.header = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
         }
-        self.ddd = ddddocr.DdddOcr(show_ad=False)
+        # self.ddd = ddddocr.DdddOcr(show_ad=False)
         # self.pages = True
 
     def start_requests(self):
@@ -47,18 +47,18 @@ class first_spider(Manager):
             # self.send_message(i)
 
     def parse(self, response):
-        print('parse状态码：', response.status_code)
-        yield MyRequests(url=response.url, headers=self.header, callback=self.ceshi)
+        self.logger.info('parse状态码：', response.status_code)
+        # yield MyRequests(url=response.url, headers=self.header, callback=self.ceshi)
 
-    def ceshi(self, response):
-        if response.status_code == None:
-            yield MyRequests(url=response.url, headers=self.header, callback=self.ceshi)
-        else:
-            print('ceshi状态码：', response.status_code)
+    # def ceshi(self, response):
+    #     if response.status_code == None:
+    #         yield MyRequests(url=response.url, headers=self.header, callback=self.ceshi)
+    #     else:
+    #         print('ceshi状态码：', response.status_code)
 
-    def parse_only(self, body):
-        time.sleep(random.uniform(2, 3))
-        print(body)
+    # def parse_only(self, body):
+    #     time.sleep(random.uniform(2, 3))
+    #     print(body)
 
 
 if __name__ == '__main__':
