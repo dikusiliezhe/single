0976a012a42afcc87a1e7605093b7efd89192bc9
dikusiliezhe <133321449@qq.com>
2023-06-29 09:28:35 +0800

    qimai test

--- spider/first_spider.py
+++ spider/first_spider.py
@@ -21,33 +21,35 @@ class first_spider(Manager):
     name = 'first_spider'
     custom_settings = {
         # 'retry_http_codes': [202, 412],
-        'Waiting_time': 100,
+        'Waiting_time': 10,
         'IS_PROXY': True,
         'IS_SAMEIP': False,
         'UA_PROXY': False,
         # 'X_MAX_PRIORITY': 15,
-        'max_request': 1,
+        # 'max_request': 1,
         # 'PREFETCH_COUNT': 50
     }
 
     def __init__(self):
         Manager.__init__(self)
-        # self.online = True
+        self.online = True
         self.header = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
         }
         # self.ddd = ddddocr.DdddOcr(show_ad=False)
-        # self.pages = True
+        # self.pages = 10
+        # self.logger.info('111111111111111111'+self.pages)
 
     def start_requests(self):
-        for i in range(100):
+        # self.logger.info('222222222222222222'+self.pages)
+        for i in range(2):
             url = 'https://www.baidu.com'
             yield MyRequests(url=url, headers=self.header, callback=self.parse)
 
             # self.send_message(i)
 
     def parse(self, response):
-        self.logger.info('parse状态码：', response.status_code)
+        self.logger.info('parse状态码：')
         # yield MyRequests(url=response.url, headers=self.header, callback=self.ceshi)
 
     # def ceshi(self, response):
