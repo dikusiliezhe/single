6a1aabd5aa7c9c77ad2110e73ec1cbc708d41be7
dikusiliezhe <133321449@qq.com>
2023-07-13 15:24:04 +0800

    test

--- spiders/hongcan/hongcan_kafka.py
+++ spiders/hongcan/hongcan_kafka.py
@@ -19,6 +19,7 @@ class HongcanKafkaSpider(Manager):
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
         }
 
+
     def start_requests(self):
         cursor = 0
         while True:
